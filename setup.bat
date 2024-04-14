b39ccdf6d39a220dd3b0aff15a7611cf98915ef3
Anton <burckhardt.anton@gmail.com>
2024-04-14 22:59:02 +1000

    Renamed filtering buttons and fixed buttons

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Setup Script for Windows Users
+
+
+REM Source and Terminology Folders created within pythonProject
+mkdir pythonProject\source
+mkdir pythonProject\terminology
+
+REM Install dependencies for the Project
+pip install Flask
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Source and Terminology Folders created within pythonProject
+mkdir -p source
+mkdir -p terminology
+
+# Install dependencies for the Project
+pip install Flask
